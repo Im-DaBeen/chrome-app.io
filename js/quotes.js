e562f9a77cd3e5ef52a8d02d4ac6efeb5d1b7eb0
Im-DaBeen <110211565+Im-DaBeen@users.noreply.github.com>
2022-10-24 04:31:30 +0900

    get quote list

--- js/quotes.js
+++ js/quotes.js
@@ -1,13 +1,45 @@
 const quotes = [
 
 {
-    quote: "hi",
-    author: "me",
+    quote: "Just believe in yourself. Even if you don’t, pretend that you do and, at some point, you will.",
+    author: "me VENUS WILLIAMS",
 },
 {
-    quote: "yo",
-    auther: "what's up?"
-}
+    quote: "If you think you can do a thing or think you can’t do a thing, you’re right.",
+    auther: "HENRY FORD"
+},
+{
+    quote:"Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will spend its whole life believing that it is stupid.",
+    auther:"Albert Einstein",
+},
+{
+    quote:"Efforts and courage are not enough without purpose and direction.",
+    auther:"JOHN F. KENNEDY",
+},
+{
+    quote:"I don’t measure a man’s success by how high he climbs, but how high he bounces when he hits the bottom.",
+    auther:"GENERAL GEORGE S. PATTON",
+},
+{
+    quote:"Never give up, for that is just the place and time that the tide will turn.",
+    auther:"HARRIET BEECHER STOWE",
+},
+{
+    quote:"The secret of getting ahead is getting started.",
+    auther:"MARK TWAIN",
+},
+{
+    quote:"If we wait for the moment when absolutely everything is ready we shall never begin.",
+    auther:"Ivan Turgenev",
+},
+{
+    quote:"By failing to prepare you are preparing to fail.",
+    auther:"Benjamin Franklin",
+},
+{
+    quote:"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
+    auther:"Thomas A. Edison",
+},
 ];
 
 const quote = document.querySelector("#quote span:first-child");
